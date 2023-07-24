cd25973f03a994a770f6ae539958c75df2a2211c
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-24 11:52:12 -0400

    Add select options for stance

---
+++ src/components/App/Form/Form.js
@@ -0,0 +1,21 @@
+const Form = () => {
+  const [stance, setStance] = useState('')
+  const [trickName, setTrickName] = useState('')
+  const [obstacle, setObstacle] = useState('')
+  const [tutorial, setTutorial] = useState('')
+
+  const handleChange = (value, setter) => {
+    setter(value)
+  }
+  
+  return (
+    <form>
+      <select onChange={() => handleChange(e.target.value, setStance)}>
+        <option value='regular'>Regular</option>
+        <option value='switch'>Switch</option>
+      </select>
+    </form>
+  )
+}
+
+export default Form 
