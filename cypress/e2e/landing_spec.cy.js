ee9c8c134a2351e90e9f7bd803324e6a927460b2
Laura Guerra <l.garciaguerra1@gmail.com>
2023-07-24 12:49:44 -0400

    Test adding a new trick

--- cypress/e2e/landing_spec.cy.js
+++ cypress/e2e/landing_spec.cy.js
@@ -4,10 +4,10 @@ describe('empty spec', () => {
       statusCode: 200,
       fixture: 'tricks.json'
     }).as('getTricks')
+    cy.visit('http://localhost:3000')
   })
   
   it('should display tricks and a form when the page loads', () => {
-    cy.visit('http://localhost:3000')
     cy.wait('@getTricks').then((interception) => {
       cy.get('h1').contains('Sick Trick Wish List')
         .get("form").children().should('have.length', 5)
@@ -20,6 +20,24 @@ describe('empty spec', () => {
         .get('.single-trick').last().contains('p', 'Obstacle: ledge')
         .get('.single-trick').last().contains('p', 'Link to tutorial:')
         .get('.single-trick').last().contains('a', 'https://www.youtube.com/watch?v=9N9swrZU1HA')
+      })
+  })
+
+  it('should reflect values input into form')
+
+  it('should display a new trick after adding it via the form', () => {
+    cy.wait('@getTricks').then((interception) => {
+      cy.get('h1').contains('Sick Trick Wish List')
+        .get('select').first().select('regular')
+        .get('input[type="text"]').first().type('new trick')
+        .get('select').last().select('stairs')
+        .get('input[type="text"]').last().type('www.example.com')
+        .get('button').contains('Send it!').click()
+        .get('.tricks-box').children().should('have.length', 4)
+        .get('.single-trick').last().contains('p', 'regular new trick')
+        .get('.single-trick').last().contains('p', 'Obstacle: stairs')
+        .get('.single-trick').last().contains('p', 'Link to tutorial:')
+        .get('.single-trick').last().contains('a', 'www.example.com')
       })
   })
 })
